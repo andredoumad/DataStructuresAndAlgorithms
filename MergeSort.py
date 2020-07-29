027f4019f04a2ce05b27d0a220e45a2503619c69
tr3b <andre@stringkeeper.com>
2020-07-28 17:16:13 -0700

    Andre Doumad

---
+++ MergeSort.py
@@ -0,0 +1,34 @@
+# Andre Doumad
+
+def MergeSort(n):
+    if len(n) > 1:
+        i_mid = len(n)//2
+        leftList = n[:i_mid]
+        rightList = n[i_mid:]
+
+        MergeSort(leftList)
+        MergeSort(rightList)
+
+        i_left=i_right=i_new = 0
+
+        while i_left < len(leftList) and i_right < len(rightList):
+            if leftList[i_left] < rightList[i_right]:
+                n[i_new] = leftList[i_left]
+                i_left+=1
+            else:
+                n[i_new] = rightList[i_right]
+                i_right+=1
+            i_new+=1
+        
+        while i_left < len(leftList):
+            n[i_new] = leftList[i_left]
+            i_left+=1
+            i_new+=1
+        
+        while i_right < len(rightList):
+            n[i_new] = rightList[i_right]
+            i_right+=1
+            i_new+=1
+        return n
+
+print(MergeSort([12,99,33,22,100,44,2,0,1,200,300,233,428,5]))
